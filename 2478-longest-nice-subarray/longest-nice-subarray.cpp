766268624e2efd35f94695f7d9a7e2e806357f47
Priyanshu74-mau <priyanshumourya731@gmail.com>
2025-03-18 23:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (87.70%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -3,20 +3,38 @@ public:
     int longestNiceSubarray(vector<int>& nums) {
        int n=nums.size();
        int ans=0;
-       for(int i=0; i<n; i++)
-       {   
-        int mask=0;
-        for(int j=i; j<n; j++)
-        {
-            if((mask & nums[j]) != 0)
-            {
-                break;
-            }
-            ans=max(ans,j-i+1);
+       // time complexity is O(n^2)
+    //    for(int i=0; i<n; i++)
+    //    {   
+    //     int mask=0;
+    //     for(int j=i; j<n; j++)
+    //     {
+    //         if((mask & nums[j]) != 0)
+    //         {
+    //             break;
+    //         }
+    //         ans=max(ans,j-i+1);
 
-            mask=(mask | nums[j]);
-        }
+    //         mask=(mask | nums[j]);
+    //     }
+    //    }
+    //    return ans; 
+
+    // using sliding window O(n)
+      int i=0;
+      int j=0;
+      int mask=0;
+      while(j<n)
+      {
+         while((mask & nums[j]) != 0)
+         {
+            mask=(mask ^ nums[i]);
+            i++;
+         }
+         ans=max(ans,j-i+1);
+         mask= (mask | nums[j]);
+         j++;
        }
-       return ans; 
+      return ans;
     }
 };
